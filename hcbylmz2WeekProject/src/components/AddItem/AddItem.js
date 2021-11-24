6a356a2e9192ab9001e0e7aed5ead9716a51aeff
Hicabi YILMAZ <yilmazhicabi@gmail.com>
2021-11-24 12:14:24 +0300

    Component Logic changed again

--- hcbylmz2WeekProject/src/components/AddItem/AddItem.js
+++ hcbylmz2WeekProject/src/components/AddItem/AddItem.js
@@ -10,8 +10,8 @@ import {
 } from 'react-native';
 import ItemSeperator from './ItemSeperator';
 import ButtonComponent from '../ButtonComponent/ButtonComponent';
-
-function AddItem() {
+import ListItemStyle from '../ListItem/ListItem.style'
+function AddItem({onPressAsc}) {
   const [itemName, setItemName] = useState('Test Item');
   const [itemPrice, setItemPrice] = useState('150');
   const [list, setList] = useState([
@@ -24,6 +24,7 @@ function AddItem() {
       alert('Please insert required field...');
     }
   }
+  onPressAsc(list);
   function renderItem({item}) {
     return (
       <View style={AddItemStyle.item}>
@@ -33,9 +34,36 @@ function AddItem() {
     );
   }
 
+  function onPressAsc() {
+    console.log("deneme");
+  }
+
+  function onPressDesc() {}
+
+  function onPressDate() {}
   return (
     <View style={AddItemStyle.container}>
-    
+      <View style={ListItemStyle.container}>
+      <View style={ListItemStyle.buttonContainer}>
+        <ButtonComponent
+          text={'Artan Fiyat'}
+          style={ListItemStyle.button}
+          textStyle={ListItemStyle.buttonText}
+          onPress={onPressAsc}
+        />
+        <ButtonComponent
+          text={'Azalan Fiyat'}
+          style={ListItemStyle.button}
+          textStyle={ListItemStyle.buttonText}
+          onPress={onPressDesc}
+        />
+        <ButtonComponent
+          text={'Tarih'}
+          style={ListItemStyle.button}
+          textStyle={ListItemStyle.buttonText}
+          onPress={onPressDate}
+        />
+      </View>
       <View style={AddItemStyle.listContainer}>
             <FlatList data={list} renderItem={renderItem} ItemSeparatorComponent={ItemSeperator} />
       </View>
@@ -54,7 +82,8 @@ function AddItem() {
         />
           <ButtonComponent text={"Add"} style={AddItemStyle.button} textStyle={AddItemStyle.buttonText} onPress={onPress}/>
        
-      </View>
+          </View>
+          </View>
     </View>
   );
 }
--- hcbylmz2WeekProject/src/components/ListItem/ListItem.js
+++ hcbylmz2WeekProject/src/components/ListItem/ListItem.js
@@ -1,47 +1,16 @@
 import React, {useState} from 'react';
-import ListItemStyle from './ListItem.style';
+
 import AddItem from '../AddItem/AddItem';
-import {
-  TouchableHighlight,
-  TextInput,
-  Text,
-  View,
-  FlatList,
-} from 'react-native';
-import ButtonComponent from '../ButtonComponent/ButtonComponent';
+
 
 function ListItem() {
-  function onPressAsc() {}
-
-  function onPressDesc() {}
-
-  function onPressDate() {}
+ 
 
   return (
-    <View style={ListItemStyle.container}>
-      <View style={ListItemStyle.buttonContainer}>
-        <ButtonComponent
-          text={'Artan Fiyat'}
-          style={ListItemStyle.button}
-          textStyle={ListItemStyle.buttonText}
-          onPress={onPressAsc}
-        />
-        <ButtonComponent
-          text={'Azalan Fiyat'}
-          style={ListItemStyle.button}
-          textStyle={ListItemStyle.buttonText}
-          onPress={onPressDesc}
-        />
-        <ButtonComponent
-          text={'Tarih'}
-          style={ListItemStyle.button}
-          textStyle={ListItemStyle.buttonText}
-          onPress={onPressDate}
-        />
-      </View>
+    
 
-      <AddItem />
-    </View>
+      <AddItem/>
+   
   );
 }
 
