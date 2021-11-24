320d2dcc70312ed916419d349e911ed7c2c581c2
Hicabi YILMAZ <yilmazhicabi@gmail.com>
2021-11-24 21:14:55 +0300

    Dynamically sort logic added

--- hcbylmz2WeekProject/src/components/AddItem/AddItem.js
+++ hcbylmz2WeekProject/src/components/AddItem/AddItem.js
@@ -1,37 +1,41 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import AddItemStyle from './AddItem.style';
-import {
-  TouchableOpacity,
-  TextInput,
-  Text,
-  View,
-  FlatList,
-
-} from 'react-native';
+import {TouchableOpacity, TextInput, Text, View, FlatList} from 'react-native';
 import ItemSeperator from './ItemSeperator';
 import ButtonComponent from '../ButtonComponent/ButtonComponent';
-import ListItemStyle from '../ListItem/ListItem.style'
+import ListItemStyle from '../ListItem/ListItem.style';
 function AddItem() {
   const [itemName, setItemName] = useState('Test Item');
   const [itemPrice, setItemPrice] = useState('150');
   const [itemId, setItemId] = useState(0);
+  const [sortState, setSortState] = useState('Date');
   const [list, setList] = useState([
-    {itemName: itemName, itemPrice: itemPrice,itemId:itemId},
+    {itemName: itemName, itemPrice: itemPrice, itemId: itemId},
   ]);
   function onPress() {
     if (itemName !== '' && itemPrice !== '') {
       newItemId = itemId;
- 
       newItemId++;
-     
       setItemId(newItemId);
-      console.log(list);
-      setList([...list, {itemName: itemName, itemPrice: itemPrice,itemId:newItemId}]);
+      let newList = [
+        ...list,
+        {itemName: itemName, itemPrice: itemPrice, itemId: newItemId},
+      ];
+
+      if (sortState == 'Ascending') {
+        sortItem('itemPrice', true, newList);
+      }
+      if (sortState == 'Descending') {
+        sortItem('itemPrice', false, newList);
+      }
+      if (sortState == 'Date') {
+        sortItem('itemId', true, newList);
+      }
     } else {
       alert('Please insert required field...');
     }
   }
- 
+
   function renderItem({item}) {
     return (
       <View style={AddItemStyle.item}>
@@ -40,68 +44,88 @@ function AddItem() {
       </View>
     );
   }
+  function sortItem(comparison, asc, newList) {
+    newList ? newList : (newList = [...list]);
+    if (comparison == 'itemPrice' && asc) {
+      newList.sort((a, b) => a.itemPrice - b.itemPrice);
+      setList(newList);
+    }
+    if (comparison == 'itemPrice' && !asc) {
+      newList.sort((a, b) => b.itemPrice - a.itemPrice);
+      setList(newList);
+    }
+    if (comparison == 'itemId' && asc) {
+      newList.sort((a, b) => a.itemId - b.itemId);
+      setList(newList);
+    }
+  }
 
   function onPressAsc() {
-  let newList = [...list];
-  newList.sort((a, b) => a.itemPrice - b.itemPrice);
-  setList(newList);
+    sortItem('itemPrice', true);
+    setSortState('Ascending');
   }
 
   function onPressDesc() {
-    let newList = [...list];
-    newList.sort((a, b) => b.itemPrice - a.itemPrice);
-    setList(newList);
+    sortItem('itemPrice', false);
+    setSortState('Descending');
   }
 
   function onPressDate() {
-    let newList = [...list];
-    newList.sort((a, b) => a.itemId - b.itemId);
-    setList(newList);
- 
+    sortItem('itemId', true);
+    setSortState('Date');
   }
+
   return (
     <View style={AddItemStyle.container}>
       <View style={ListItemStyle.container}>
-      <View style={ListItemStyle.buttonContainer}>
-        <ButtonComponent
-          text={'Artan Fiyat'}
-          style={ListItemStyle.button}
-          textStyle={ListItemStyle.buttonText}
-          onPress={onPressAsc}
-        />
-        <ButtonComponent
-          text={'Azalan Fiyat'}
-          style={ListItemStyle.button}
-          textStyle={ListItemStyle.buttonText}
-          onPress={onPressDesc}
-        />
-        <ButtonComponent
-          text={'Tarih'}
-          style={ListItemStyle.button}
-          textStyle={ListItemStyle.buttonText}
-          onPress={onPressDate}
-        />
+        <View style={ListItemStyle.buttonContainer}>
+          <ButtonComponent
+            text={'Artan Fiyat'}
+            style={ListItemStyle.button}
+            textStyle={ListItemStyle.buttonText}
+            onPress={onPressAsc}
+          />
+          <ButtonComponent
+            text={'Azalan Fiyat'}
+            style={ListItemStyle.button}
+            textStyle={ListItemStyle.buttonText}
+            onPress={onPressDesc}
+          />
+          <ButtonComponent
+            text={'Tarih'}
+            style={ListItemStyle.button}
+            textStyle={ListItemStyle.buttonText}
+            onPress={onPressDate}
+          />
+        </View>
+        <View style={AddItemStyle.listContainer}>
+          <FlatList
+            data={list}
+            renderItem={renderItem}
+            ItemSeparatorComponent={ItemSeperator}
+          />
+        </View>
+        <View style={AddItemStyle.inputContainer}>
+          <Text>Name</Text>
+          <TextInput
+            style={AddItemStyle.input}
+            placeholder="Şapka"
+            onChangeText={setItemName}
+          />
+          <Text>Price</Text>
+          <TextInput
+            style={AddItemStyle.input}
+            placeholder="150"
+            onChangeText={setItemPrice}
+          />
+          <ButtonComponent
+            text={'Add'}
+            style={AddItemStyle.button}
+            textStyle={AddItemStyle.buttonText}
+            onPress={onPress}
+          />
+        </View>
       </View>
-      <View style={AddItemStyle.listContainer}>
-            <FlatList data={list} renderItem={renderItem} ItemSeparatorComponent={ItemSeperator} />
-      </View>
-      <View style={AddItemStyle.inputContainer}>
-        <Text>Name</Text>
-        <TextInput
-          style={AddItemStyle.input}
-          placeholder="Şapka"
-          onChangeText={setItemName}
-        />
-        <Text>Price</Text>
-        <TextInput
-          style={AddItemStyle.input}
-          placeholder="150"
-          onChangeText={setItemPrice}
-        />
-          <ButtonComponent text={"Add"} style={AddItemStyle.button} textStyle={AddItemStyle.buttonText} onPress={onPress}/>
-       
-          </View>
-          </View>
     </View>
   );
 }
